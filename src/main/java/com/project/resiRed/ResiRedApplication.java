c8f4d44fd0bcf0120a1059c67aeaac6d9b75c55f
nicolebernal <nicolemichelle120@gmail.com>
2024-03-16 16:04:47 -0500

    Mi primer commit: Subiendo base de datos y etsructura basica

---
+++ src/main/java/com/project/resiRed/ResiRedApplication.java
@@ -0,0 +1,13 @@
+package com.project.resiRed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiRedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/resiRed/ResiRedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.resiRed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiRedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
