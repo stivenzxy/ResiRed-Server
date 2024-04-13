f02aa921ff8b90e9bbd8e06afa9983206165bae5
Daniel MC <danielcarrillo3200@gmail.com>
2024-04-13 17:50:46 -0500

    changes

--- src/main/java/com/project/resiRed/ResiRedApplication.java
+++ src/main/java/com/project/resiRed/ResiRedApplication.java
@@ -3,7 +3,9 @@ package com.project.resiRed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableMethodSecurity
 @SpringBootApplication
 public class ResiRedApplication {
 
