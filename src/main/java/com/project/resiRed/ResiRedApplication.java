55480c9a3db54786cd0887e05f253a8989d5b526
Stivenzxy <jesuspereztorres420@gmail.com>
2024-04-15 22:38:52 -0500

    cambios pequeños

--- src/main/java/com/project/resiRed/ResiRedApplication.java
+++ src/main/java/com/project/resiRed/ResiRedApplication.java
@@ -2,7 +2,6 @@ package com.project.resiRed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableMethodSecurity
--- src/main/java/com/project/resiRed/service/JwtService.java
+++ src/main/java/com/project/resiRed/service/JwtService.java
@@ -48,7 +48,7 @@ public class JwtService {
                 .claim("type", "ACCESS_TOKEN")
                 .subject(user.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10)) // el token de acceso cambia cada 10 mins
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) // el token de acceso cambia cada 10 mins
                 .signWith(getKey())
                 .compact();
     }
@@ -71,7 +71,7 @@ public class JwtService {
             RefreshToken newToken = new RefreshToken();
             newToken.setToken(refreshToken);
             newToken.setUser(user);
-            newToken.setExpirationDate(new Date(System.currentTimeMillis() + 1000 * 60 * 5));
+            newToken.setExpirationDate(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 3));
             refreshTokenRepository.save(newToken);
         }
 
